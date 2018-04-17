4abe74d753ef085af339969b2aaeb69facab9670
Leevi Aaltonen <leevia00@gmail.com>
2018-04-17 17:27:47 +0300

    Added string parameter handling

--- excavator.py
+++ excavator.py
@@ -2,6 +2,16 @@
 import pathlib
 
 
+def makepath(path):
+    if isinstance(path, pathlib.Path):
+        return path
+    else:
+        try:
+            return pathlib.Path(path)
+        except:
+            raise
+
+
 def excavate(origin, destination):
     """
     Recursively moves all files from all subdirectories of one specified directory to another.
@@ -12,6 +22,8 @@ def excavate(origin, destination):
     destination: Path
         The directory the files will be moved to
     """
+    origin = makepath(origin)
+    destination = makepath(destination)
     duplicates = {}
     for path in origin.iterdir():
         if path.is_dir():
